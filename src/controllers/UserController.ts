d1ec25ea7eed3775ea7f121a8736b5d0e38b1325
Oussema Heni <oussemaheny2@gmail.com>
2024-08-31 12:51:26 +0100

    feat: add CRUD operations for user collection, needs test - wip

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,6 +1,20 @@
 import { RequestHandler } from "express";
 import UserModel from "../models/User";
 import { IUser } from "../interface/IUser";
+import { isValidObjectId } from "mongoose";
+
+export const getUsers: RequestHandler = async (req, res) => {
+  try {
+    const users = (await UserModel.find()) ?? [];
+
+    return res.status(200).json({ data: users });
+  } catch (error: any) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ message: "Internal Server Error, Could not delete user" });
+  }
+};
 
 export const addUser: RequestHandler = async (req, res) => {
   try {
@@ -17,9 +31,52 @@ export const addUser: RequestHandler = async (req, res) => {
 
     const user = new UserModel(userData);
     const savedUser = await user.save();
-    res.status(200).json(savedUser);
+    res.status(201).json({ message: "User created", data: savedUser });
   } catch (error: any) {
     console.error(error);
     res.status(400).json({ message: "Could not save user, please try again" });
+  }
+};
+
+export const updateUser: RequestHandler = async (req, res) => {
+  try {
+    const { userId } = req.params;
+    if (!isValidObjectId(userId)) {
+      return res.status(400).json({ message: "Invalid User ID" });
+    }
+
+    const userData: Partial<Omit<IUser, "password" | "id" | "_id">> = {
+      ...req.body,
+    };
+    const updatedUser = await UserModel.findByIdAndUpdate(userId, userData, {
+      runValidators: true,
+    });
+
+    if (!updatedUser) {
+      return res.status(400).json({ message: "User not found" });
+    }
+
+    res.status(201).json({ message: "User updated", data: updatedUser });
+  } catch (error: any) {
+    console.error(error);
+    res.status(400).json({ message: "Could not save user, please try again" });
+  }
+};
+
+export const deleteUser: RequestHandler = async (req, res) => {
+  try {
+    const { userId } = req.params;
+
+    if (!isValidObjectId(userId))
+      return res.status(400).json({ message: "Invalid User ID" });
+
+    const user = await UserModel.findByIdAndDelete(userId);
+
+    return res.status(200).json({ userId, user });
+  } catch (error: any) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ message: "Internal Server Error, Could not delete user" });
   }
 };
--- src/routes/UserRoutes.ts
+++ src/routes/UserRoutes.ts
@@ -1,8 +1,16 @@
 import { Router } from "express";
-import { addUser } from "../controllers/UserController";
+import {
+  addUser,
+  deleteUser,
+  getUsers,
+  updateUser,
+} from "../controllers/UserController";
 
 const router = Router();
 
+router.get("/", getUsers);
 router.post("/", addUser);
+router.patch("/", updateUser);
+router.delete("/:userId", deleteUser);
 
 export default router;
