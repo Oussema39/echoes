2148a762a77ee2601c5b029ac7c8fb91d9a8a280
Oussema Heni <oussemaheny2@gmail.com>
2024-09-15 12:21:49 +0100

    feat: add authentication middleware for private routes

---
+++ src/middlewares/authMiddleware.ts
@@ -0,0 +1,20 @@
+import { RequestHandler } from "express";
+import jwt from "jsonwebtoken";
+
+const JWT_SECRET = process.env.JWT_SECRET;
+
+export const isAuthenticated: RequestHandler = async (req, res, next) => {
+  const accessToken = req.headers.authorization?.split(" ")?.[1];
+
+  if (!accessToken) {
+    return res.status(401).json({ message: "Missing authorization token" });
+  }
+
+  try {
+    const decoded = jwt.verify(accessToken!, JWT_SECRET);
+    (req as any).user = decoded;
+    next();
+  } catch (error) {
+    res.status(401).json({ message: "Unauthorized access" });
+  }
+};
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,10 @@
 import { Router, type Router as TRouter } from "express";
 import UserRouter from "./UserRoutes";
 import AuthRouter from "./AuthRoutes";
+import { isAuthenticated } from "../middlewares/authMiddleware";
 
 const router: TRouter = Router();
-router.use("/user", UserRouter);
+router.use("/user", isAuthenticated, UserRouter);
 router.use("/auth", AuthRouter);
 
 export default router;
