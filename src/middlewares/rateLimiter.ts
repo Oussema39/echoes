883eadac521f4b569d44d9f598a31c23be5a5608
Oussema Heni <oussemaheny2@gmail.com>
2024-10-27 11:46:14 +0100

    feat: implement rate limiter && add env example

---
+++ src/middlewares/rateLimiter.ts
@@ -0,0 +1,28 @@
+import { RequestHandler } from "express";
+
+const lastUserRequest: Record<string, number> = {};
+
+export const rateLimiterMiddleware: RequestHandler = (req, res, next) => {
+  const user: TUserProps = (req as any).user;
+  if (!user) return res.status(401).json({ message: "Unauthorized access" });
+
+  const lastRequest = lastUserRequest[user.email];
+  const now = Date.now();
+  const differenceInSeconds = (now - lastRequest) / 1000;
+
+  if (!lastRequest) {
+    lastUserRequest[user.email] = now;
+    return next();
+  }
+
+  if (differenceInSeconds < 10) {
+    return res.status(429).json({
+      message:
+        "Too many requests. Please wait a few seconds before trying again.",
+    });
+  }
+
+  lastUserRequest[user.email] = now;
+
+  return next();
+};
--- src/routes/AuthRoutes.ts
+++ src/routes/AuthRoutes.ts
@@ -6,13 +6,20 @@ import {
   sendVerificationEmail,
   verifyEmail,
 } from "../controllers/AuthController";
+import { isAuthenticated } from "../middlewares/authMiddleware";
+import { rateLimiterMiddleware } from "../middlewares/rateLimiter";
 
 const router = Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.all("/verifyEmail", verifyEmail);
-router.post("/sendVerificationEmail", sendVerificationEmail);
+router.post(
+  "/sendVerificationEmail",
+  isAuthenticated,
+  rateLimiterMiddleware,
+  sendVerificationEmail
+);
 router.patch("/refreshToken", refreshAccessToken);
 
 export default router;
