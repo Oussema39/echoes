bbfd1482fae4642e5b22c414de82f9ecb99de4f2
Oussema Heni <oussemaheny2@gmail.com>
2025-03-16 12:07:18 +0100

    feat: add CORS support to the server

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,16 @@
 import express, { Application } from "express";
 import routes from "./routes/index";
+import cors from "cors";
 const app: Application = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
