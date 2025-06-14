584a7f56872a5fa4f8c561b855a6eeb1ab61fb5f
Oussema Heni <oussemaheny2@gmail.com>
2025-06-14 13:10:03 +0100

    feat: update Puppeteer executable path for production environment

--- src/config/puppeteerBrowser.ts
+++ src/config/puppeteerBrowser.ts
@@ -1,11 +1,15 @@
+import { join } from "path";
 import puppeteer, { LaunchOptions } from "puppeteer";
 
 export const buildBrowser = async (options?: Partial<LaunchOptions>) => {
   const browser = await puppeteer.launch({
     ...(process.env.NODE_ENV === "production"
       ? {
-          executablePath:
-            "C:\\Program Files\\Chromium\\Application\\chrome.exe",
+          executablePath: join(
+            __dirname,
+            "..",
+            "chrome/linux-135.0.7049.114/chrome-linux64/chrome"
+          ),
           args: ["--no-sandbox"],
         }
       : {}),
