b9badeef78863e3984857560c13ed7c836f5d391
Oussema Heni <oussemaheny2@gmail.com>
2025-06-15 12:54:46 +0100

    feat: update Puppeteer integration to use puppeteer-core and chrome-aws-lambda for improved compatibility

--- src/config/puppeteerBrowser.ts
+++ src/config/puppeteerBrowser.ts
@@ -1,18 +1,13 @@
-import puppeteer, { LaunchOptions } from "puppeteer";
+import puppeteer, { LaunchOptions } from "puppeteer-core";
+import chromium from "chrome-aws-lambda";
 
 export const buildBrowser = async (options?: Partial<LaunchOptions>) => {
+  const exePath = await chromium.executablePath;
+
   const browser = await puppeteer.launch({
-    ...(process.env.NODE_ENV === "production"
-      ? {
-          //   executablePath: join(
-          //     __dirname,
-          //     "..",
-          //     "chrome/linux-135.0.7049.114/chrome-linux64/chrome"
-          //   ),
-          args: ["--no-sandbox"],
-        }
-      : {}),
-    headless: true,
+    executablePath: exePath,
+    args: chromium.args,
+    headless: chromium.headless,
     ...options,
   });
 
